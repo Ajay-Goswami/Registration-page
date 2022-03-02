30e7f6d8822f3a55bf011cf458f1bacb52638bff
Ajay-Goswami <goswamiajay2002@gmail.com>
2022-03-02 22:18:50 +0530

    this is 1st commit of login page using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const adminLogin = {
+    email: 'admin@gmail.com',
+    username: "admin",
+    password: "admin"
+  };
+  const [user, setUser]= useState({name:"",email:""});  
+  const [error, setError ] = useState("");
+
+  const Login= details => {
+    console.log(details);
+  }
+
+  const LogOut =() =>{
+    console.log("logout");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className="welcome">
+         <h1>Welcome, <span>{user.name}</span></h1>
+         <button >Logout</button>
+        </div>
+          ):
+          (<LoginPage Login={Login} error={error}/>)
+          }
     </div>
   );
 }
